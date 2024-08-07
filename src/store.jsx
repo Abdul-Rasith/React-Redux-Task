138ecbf22502fd28793be2c6b86eed017a72be09
Abdul-Rasith <mscrasith@gmail.com>
2024-08-07 07:54:55 +0530

    Upload my react-redux task

--- src/store.jsx
+++ src/store.jsx
@@ -1,108 +1,116 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-const products = [
-  {
-    id: 1,
-    title: "iPhone 9",
-    description: "An apple mobile which is nothing like apple",
-    price: 549,
-    discountPercentage: 12.96,
-    rating: 4.69,
-    stock: 94,
-    brand: "Apple",
-    category: "smartphones",
-    thumbnail:
-      "https://indianexpress.com/wp-content/uploads/2018/07/apple_iphonex_1.jpg",
-    images: [
-      "https://indianexpress.com/wp-content/uploads/2018/07/apple_iphonex_1.jpg",
-      "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      "https://m-cdn.phonearena.com/images/article/105823-wide-two_940/Samsung-will-release-a-Galaxy-Note-9-with-512GB-of-storage-in-certain-markets.webp?1529243768",
-      "https://m.media-amazon.com/images/I/317eOgpBM7S._SX300_SY300_QL70_FMwebp_.jpg",
-      "https://360view.3dmodels.org/zoom/Huawei/Huawei_P30_lite_Peacock_Blue_1000_0001.jpg",
-    ],
-  },
-  {
-    id: 2,
-    title: "iPhone X",
-    description:
-      "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
-    price: 899,
-    discountPercentage: 17.94,
-    rating: 4.44,
-    stock: 34,
-    brand: "Apple",
-    category: "smartphones",
-    thumbnail:
-      "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-    images: [
-      "https://indianexpress.com/wp-content/uploads/2018/07/apple_iphonex_1.jpg",
-      "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      "https://m-cdn.phonearena.com/images/article/105823-wide-two_940/Samsung-will-release-a-Galaxy-Note-9-with-512GB-of-storage-in-certain-markets.webp?1529243768",
-      "https://m.media-amazon.com/images/I/317eOgpBM7S._SX300_SY300_QL70_FMwebp_.jpg",
-      "https://360view.3dmodels.org/zoom/Huawei/Huawei_P30_lite_Peacock_Blue_1000_0001.jpg",
-    ],
-  },
-  {
-    id: 3,
-    title: "Samsung Universe 9",
-    description:
-      "Samsung's new variant which goes beyond Galaxy to the Universe",
-    price: 1249,
-    discountPercentage: 15.46,
-    rating: 4.09,
-    stock: 36,
-    brand: "Samsung",
-    category: "smartphones",
-    thumbnail:
-      "https://m-cdn.phonearena.com/images/article/105823-wide-two_940/Samsung-will-release-a-Galaxy-Note-9-with-512GB-of-storage-in-certain-markets.webp?1529243768",
-    images: [
-      "https://m-cdn.phonearena.com/images/article/105823-wide-two_940/Samsung-will-release-a-Galaxy-Note-9-with-512GB-of-storage-in-certain-markets.webp?1529243768",
-    ],
-  },
-  {
-    id: 4,
-    title: "OPPO F19",
-    description: "OPPO F19 is officially announced on April 2021.",
-    price: 280,
-    discountPercentage: 17.91,
-    rating: 4.3,
-    stock: 123,
-    brand: "OPPO",
-    category: "smartphones",
-    thumbnail:
-      "https://m.media-amazon.com/images/I/317eOgpBM7S._SX300_SY300_QL70_FMwebp_.jpg",
-    images: [
-      "https://m.media-amazon.com/images/I/317eOgpBM7S._SX300_SY300_QL70_FMwebp_.jpg",
-      "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      "https://m-cdn.phonearena.com/images/article/105823-wide-two_940/Samsung-will-release-a-Galaxy-Note-9-with-512GB-of-storage-in-certain-markets.webp?1529243768",
-      "https://360view.3dmodels.org/zoom/Huawei/Huawei_P30_lite_Peacock_Blue_1000_0001.jpg",
-      "https://indianexpress.com/wp-content/uploads/2018/07/apple_iphonex_1.jpg",
-    ],
-  },
-  {
-    id: 5,
-    title: "Huawei P30",
-    description:
-      "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
-    price: 499,
-    discountPercentage: 10.58,
-    rating: 4.09,
-    stock: 32,
-    brand: "Huawei",
-    category: "smartphones",
-    thumbnail:
-      "https://360view.3dmodels.org/zoom/Huawei/Huawei_P30_lite_Peacock_Blue_1000_0001.jpg",
-    images: [
-      "https://360view.3dmodels.org/zoom/Huawei/Huawei_P30_lite_Peacock_Blue_1000_0001.jpg",
-      "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
-      "https://m-cdn.phonearena.com/images/article/105823-wide-two_940/Samsung-will-release-a-Galaxy-Note-9-with-512GB-of-storage-in-certain-markets.webp?1529243768",
-    ],
-  },
-];
+const initialState = {
+  products: [
+    {
+      id: 1,
+      title: "iPhone 9",
+      description: "An apple mobile which is nothing like apple",
+      price: 549,
+      discountPercentage: 12.96,
+      rating: 4.69,
+      stock: 94,
+      brand: "Apple",
+      category: "smartphones",
+      thumbnail:
+        "https://indianexpress.com/wp-content/uploads/2018/07/apple_iphonex_1.jpg",
+      images: [
+        "https://indianexpress.com/wp-content/uploads/2018/07/apple_iphonex_1.jpg",
+        "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://m-cdn.phonearena.com/images/article/105823-wide-two_940/Samsung-will-release-a-Galaxy-Note-9-with-512GB-of-storage-in-certain-markets.webp?1529243768",
+        "https://m.media-amazon.com/images/I/317eOgpBM7S._SX300_SY300_QL70_FMwebp_.jpg",
+        "https://360view.3dmodels.org/zoom/Huawei/Huawei_P30_lite_Peacock_Blue_1000_0001.jpg",
+      ],
+    },
+    {
+      id: 2,
+      title: "iPhone X",
+      description:
+        "SIM-Free, Model A19211 6.5-inch Super Retina HD display with OLED technology A12 Bionic chip with ...",
+      price: 899,
+      discountPercentage: 17.94,
+      rating: 4.44,
+      stock: 34,
+      brand: "Apple",
+      category: "smartphones",
+      thumbnail:
+        "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+      images: [
+        "https://indianexpress.com/wp-content/uploads/2018/07/apple_iphonex_1.jpg",
+        "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://m-cdn.phonearena.com/images/article/105823-wide-two_940/Samsung-will-release-a-Galaxy-Note-9-with-512GB-of-storage-in-certain-markets.webp?1529243768",
+        "https://m.media-amazon.com/images/I/317eOgpBM7S._SX300_SY300_QL70_FMwebp_.jpg",
+        "https://360view.3dmodels.org/zoom/Huawei/Huawei_P30_lite_Peacock_Blue_1000_0001.jpg",
+      ],
+    },
+    {
+      id: 3,
+      title: "Samsung Universe 9",
+      description:
+        "Samsung's new variant which goes beyond Galaxy to the Universe",
+      price: 1249,
+      discountPercentage: 15.46,
+      rating: 4.09,
+      stock: 36,
+      brand: "Samsung",
+      category: "smartphones",
+      thumbnail:
+        "https://m-cdn.phonearena.com/images/article/105823-wide-two_940/Samsung-will-release-a-Galaxy-Note-9-with-512GB-of-storage-in-certain-markets.webp?1529243768",
+      images: [
+        "https://indianexpress.com/wp-content/uploads/2018/07/apple_iphonex_1.jpg",
+        "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://m-cdn.phonearena.com/images/article/105823-wide-two_940/Samsung-will-release-a-Galaxy-Note-9-with-512GB-of-storage-in-certain-markets.webp?1529243768",
+        "https://m.media-amazon.com/images/I/317eOgpBM7S._SX300_SY300_QL70_FMwebp_.jpg",
+        "https://360view.3dmodels.org/zoom/Huawei/Huawei_P30_lite_Peacock_Blue_1000_0001.jpg",
+      ],
+    },
+    {
+      id: 4,
+      title: "OPPOF19",
+      description: "OPPO F19 is officially announced on April 2021.",
+      price: 280,
+      discountPercentage: 17.91,
+      rating: 4.3,
+      stock: 123,
+      brand: "OPPO",
+      category: "smartphones",
+      thumbnail:
+        "https://m.media-amazon.com/images/I/317eOgpBM7S._SX300_SY300_QL70_FMwebp_.jpg",
+      images: [
+        "https://indianexpress.com/wp-content/uploads/2018/07/apple_iphonex_1.jpg",
+        "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://m-cdn.phonearena.com/images/article/105823-wide-two_940/Samsung-will-release-a-Galaxy-Note-9-with-512GB-of-storage-in-certain-markets.webp?1529243768",
+        "https://m.media-amazon.com/images/I/317eOgpBM7S._SX300_SY300_QL70_FMwebp_.jpg",
+        "https://360view.3dmodels.org/zoom/Huawei/Huawei_P30_lite_Peacock_Blue_1000_0001.jpg",
+      ],
+    },
+    {
+      id: 5,
+      title: "Huawei P30",
+      description:
+        "Huawei’s re-badged P30 Pro New Edition was officially unveiled yesterday in Germany and now the device has made its way to the UK.",
+      price: 499,
+      discountPercentage: 10.58,
+      rating: 4.09,
+      stock: 32,
+      brand: "Huawei",
+      category: "smartphones",
+      thumbnail:
+        "https://360view.3dmodels.org/zoom/Huawei/Huawei_P30_lite_Peacock_Blue_1000_0001.jpg",
+      images: [
+        "https://indianexpress.com/wp-content/uploads/2018/07/apple_iphonex_1.jpg",
+        "https://images.pexels.com/photos/788946/pexels-photo-788946.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1",
+        "https://m-cdn.phonearena.com/images/article/105823-wide-two_940/Samsung-will-release-a-Galaxy-Note-9-with-512GB-of-storage-in-certain-markets.webp?1529243768",
+        "https://m.media-amazon.com/images/I/317eOgpBM7S._SX300_SY300_QL70_FMwebp_.jpg",
+        "https://360view.3dmodels.org/zoom/Huawei/Huawei_P30_lite_Peacock_Blue_1000_0001.jpg",
+      ],
+    },
+  ],
+};
 
 const productSlice = createSlice({
   name: "products",
-  initialState: products,
+  initialState: initialState.products,
   reducers: {},
 });
 
